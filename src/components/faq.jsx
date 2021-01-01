889ef0c016b96708441e0f720f4de2edfdceab8c
Daniel Ma <ziyuema@gmail.com>
2021-01-01 15:37:31 -0500

    basic faq functionality

--- src/components/faq.jsx
+++ src/components/faq.jsx
@@ -1,9 +1,37 @@
 import React from 'react'
+import Accordion from 'react-bootstrap/Accordion'
+import Card from 'react-bootstrap/Card'
+
+import { faq } from './content.json'
+
+const FAQitem = ({q, a, link, textb, textin, texta, i}) => {
+  return (
+    <Card>
+      <Accordion.Toggle as={Card.Header} eventKey={i}>
+        {q}
+      </Accordion.Toggle>
+      <Accordion.Collapse eventKey={i}>
+        <Card.Body>
+          {link ? (
+            <> {textb}<a href={link}>{textin}</a>{texta} </>
+          ) : (a)}
+        </Card.Body>
+      </Accordion.Collapse>
+    </Card>
+  )
+}
 
 function FAQ() {
   return (
     <div id='faq'>
-
+      <h2>Frequently Asked Questions</h2>
+      <div>
+        <Accordion>
+          {faq.map((item, i) =>(
+            <FAQitem {...item} i={i} key={i}/>
+          ))}
+        </Accordion>
+      </div>
     </div>
   )
 }
