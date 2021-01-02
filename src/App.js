831689d81036a69e57aeaebd2fc3312c865eeec6
dilan-js <dilannana.js@gmail.com>
2021-01-02 14:14:26 -0600

    finished basic about

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
-import React from 'react';
-import Navbar from './components/navbar';
-import Home from './components/home';
-import About from './components/about';
-import Tracks from './components/tracks';
-import FAQ from './components/faq';
-import Sponsors from './components/sponsors';
-import Footer from './components/footer';
+import React from "react";
+import Navbar from "./components/navbar";
+import Home from "./components/home";
+import About from "./components/about";
+import Tracks from "./components/tracks";
+import FAQ from "./components/faq";
+import Sponsors from "./components/sponsors";
+import Footer from "./components/footer";
+import DilanAbout from "./components/About/About";
 
 function App() {
   return (
     <div>
-      <Navbar/>
-      <Home/>
-      <About/>
-      <Tracks/>
-      <FAQ/>
-      <Sponsors/>
-      <Footer/>
+      <Navbar />
+      <Home />
+      <DilanAbout />
+      <About />
+      <Tracks />
+      <FAQ />
+      <Sponsors />
+      <Footer />
     </div>
   );
 }
