4fe3e082c0cecda5b4f91f7cfeb029b4c31589c7
Daniel Ma <ziyuema@gmail.com>
2021-02-23 22:06:12 -0700

    application deadlines

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -8,6 +8,7 @@ function Home() {
       <div id="home-content">
         <h1 id="Title">Stanford Open Datathon 2021</h1>
         <h4>April 9-11, 2021 Hosted by The Stanford Open Data Project</h4>
+        <h5>Applications due 3/15!</h5>
         <div>
           <Button variant="primary shadow-none">
             <a href="https://docs.google.com/forms/d/e/1FAIpQLSekpg0-j0vEGCWg9ZM534NdDofMMaJggohJXFv6b1MrGPUYpw/viewform" 
@@ -15,6 +16,7 @@ function Home() {
           </Button>
           <Button variant="outline-primary shadow-none"><a href="http://eepurl.com/hnqk9H" target="_blank" rel="noopener noreferrer">Keep Me Updated</a></Button>
         </div>
+        
       </div>
       <img id="Hero" src={Hero} alt="hero"/>
     </div>
