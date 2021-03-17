d76c1313d633d67058e68c2ddae03b76b670937f
Daniel Ma <ziyuema@gmail.com>
2021-03-17 01:56:25 -0600

    Sponsors

--- src/components/sponsors.jsx
+++ src/components/sponsors.jsx
@@ -4,23 +4,25 @@ import { Spon } from './content.json'
 
 const logos = {
   "CIF": require("../Images/Sponsors/CIF.png"),
+  "EST": require("../Images/Sponsors/EST.png"),
+  "USAFacts": require("../Images/Sponsors/USAFacts.png"),
+  "CS": require("../Images/Sponsors/CS.png"),
   "SEA": require("../Images/Sponsors/SEA.jpeg"),
   "SIEPR": require("../Images/Sponsors/SIEPR.png")
 }
 
 function Sponsors() {
-  const Sponsor = ({name, link}) => {
+  const Sponsor = ({name, link, size}) => {
     return (
-      <a href={link}>
+      <a href={link} className={`${size}`}>
         <img src={logos[name]} alt={logos[name] + "logo"} className="sponsor"/>
       </a>
     );
   };
   return (
     <div id="sponsors">
-      <h2 class="section-heading">Sponsors Coming Soon!</h2>
-      <Button  variant="outline-primary shadow-none"><a href="mailto:stanfordopendataproject@gmail.com?Subject=Sponsorship%20Interest">Become A Sponsor!</a></Button>
-      {/*<div>
+      <h2 class="section-heading">Sponsors</h2>
+      <div id="images">
         {Spon.map((item) => (
           <Sponsor {...item}/>
         ))}
